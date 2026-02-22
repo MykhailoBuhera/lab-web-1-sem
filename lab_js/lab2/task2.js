a57bd579ea9fa50609f81e9eebc90f8987ce5a32
Mykhailo Buhera <mihapvp1990@gmail.com>
2026-02-22 21:03:21 +0200

    task2

---
+++ lab_js/lab2/task2.js
@@ -0,0 +1,15 @@
+let a = 15;
+
+console.log(a);
+let b;
+    if (a > 10 && a < 20) {
+        b = !false;
+    } else {
+        b = !true;
+    }
+
+    if (b) {
+        console.log("The number is in the range 10-20.");
+    } else {
+        console.log("The number is not in the range 10-20.");
+    }
