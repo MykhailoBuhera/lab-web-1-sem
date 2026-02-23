7a28e2cc3988ba1440185ca02a5335006c29f555
Mykhailo Buhera <mihapvp1990@gmail.com>
2026-02-23 12:34:54 +0200

    end

--- lab_js/lab2/task1.js
+++ lab_js/lab2/task1.js
@@ -5,15 +5,18 @@ let arr = [size];
 for (let i = 0; i < size; i++) {
     arr[i] = Math.floor(Math.random() * 100);
     console.log(arr[i]);}
+function getMinMax(arr) {
+    for (let i = 0; i < size; i++) {
+        if (arr[i] <min){
+            min = arr[i];
+        }
+    }
 
-for (let i = 0; i < size; i++) {
-    if (arr[i] <min){
-        min = arr[i];
-    }}
-console.log("Min: " + min);
-
-for (let i = 0; i < size; i++) {
-    if(arr[i]>max){
-        max = arr[i];
-    }}
-console.log("Max: " + max);
+    for (let i = 0; i < size; i++) {
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+    return {min, max};
+}
+console.log(getMinMax(arr));
--- lab_js/lab2/task2.js
+++ lab_js/lab2/task2.js
@@ -1,15 +1,18 @@
 let a = Math.floor(Math.random() * 30);
-
 console.log(a);
-let b;
-    if (a > 10 && a < 20) {
+
+function checkRange(a) {
+    let b;
+if (a > 10 && a < 20) {
         b = !false;
     } else {
         b = !true;
     }
 
-    if (b) {
-        console.log("The number is in the range 10-20.");
+if (b) {
+      return console.log("The number is in the range 10-20.");
     } else {
-        console.log("The number is not in the range 10-20.");
+        return console.log("The number is not in the range 10-20.");
     }
+}
+checkRange(a);
